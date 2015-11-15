95f88536254bf4775b9ebf11108735e09c2008fe
Gary Mixson <prevnode@gmail.com>
2015-11-15 00:07:11 -0800

    hooks for new states

---
+++ Assets/Resources/Scripts/AI/AIState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AIState : MonoBehaviour {
+
+    private GameObject _owner;
+
+    public abstract void StateUpdate();
+
+    public abstract void StateStart();
+
+    public abstract void StateEnd();
+
+    public virtual void SetOwner(GameObject owner)
+    {
+        _owner = owner;
+    }
+
+    
+  
+        
+}
